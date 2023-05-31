7ebc14f3cb8686685339ccab02db5469b0e44c27
Felipe Scarel <fbscarel@gmail.com>
2023-05-31 09:34:14 -0300

    change kubeadm keyinstall procedure; dos2unix full repo

--- s2/setup.sh
+++ s2/setup.sh
@@ -159,7 +159,7 @@ swapoff -a
 sed -i 's/^\(.*vg-swap.*\)/#\1/' /etc/fstab
 
 # Install kubeadm and friends
-curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 apt update
 apt install -y kubelet=${K8S_VERSION} \
